92dfe0a0b92614defa01b0981e2a211f5d8d5fc9
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-03-29 00:35:58 +0530

    video correction

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import video from '../assets/vid8.mp4'
+import React from "react";
+import video from "../assets/vid8.mp4";
 
 const Dashboard = () => {
-
-
   return (
-    <main style={{height: "75vh"}}>
-  <div className="position-relative overflow-hidden text-center bg-body-light h-100">
-    <video muted autoPlay loop  id='myVideo'><source src={video} type="video/mp4" /></video>
-    <div className="col-md-6 p-lg-5 mx-auto my-5">
-      <h1 className="display-3 fw-bold mt-5 pt-5">Building Dreams, One Brick at a Time</h1>
-      <h3 className="fw-normal text-muted mb-3 ">Crafting Homes, Creating Memories</h3>
-    </div>
-  </div>
-</main>
-  )
-}
+    <main style={{ height: "75vh" }}>
+      <div className="position-relative overflow-hidden text-center bg-body-light h-100">
+        <video src={video} autoPlay loop muted id="myVideo"></video>
+        <div className="col-md-6 p-lg-5 mx-auto my-5">
+          <h1
+            className="display-3 fw-bold mt-5 pt-5"
+            style={{ color: "var(--background-color)" }}
+          >
+            Building Dreams, One Brick at a Time
+          </h1>
+          <h3 className="fw-normal text-muted mb-3 ">
+            Crafting Homes, Creating Memories
+          </h3>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
